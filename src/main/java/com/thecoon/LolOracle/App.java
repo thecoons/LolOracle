a020dce53c84ea352f18a25773e55b601548ad2b
thecoons <antobarth@gmail.com>
2016-05-26 20:24:45 +0200

    maven update

---
+++ src/main/java/com/thecoon/LolOracle/App.java
@@ -0,0 +1,13 @@
+package com.thecoon.LolOracle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
